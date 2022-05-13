1e17feb283ffa6fd0ade6eccfeb62ba07a5be613
Radu Stoica <radux188@gmail.com>
2022-05-13 15:50:59 +0300

    Mail

---
+++ router/messagesRouter.js
@@ -0,0 +1,175 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+const connection = require('../db');
+const {detectLanguage, translateText} = require("../utils/translateFunctions")
+const {sendMail } = require("../utils/mailFunctions");
+const {LANGUAGE_ISO_CODE} = require("../utils/dictionaries");
+//Get all messages
+router.get("/", (req, res) => {
+    connection.query("SELECT * FROM messages", (err, results) =>{
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+
+        return res.json({
+            messages: results,
+        })
+    })
+});
+
+//get a message by id
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    connection.query(`SELECT * FROM messages where entryID = ${mysql.escape(id)}`, (err, results) =>{
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+
+        if (!results.length) {
+            return res.status(400).json({
+                error: "Message not found",
+            })
+        }
+
+        return res.json({
+            messages: results,
+        })
+    })
+});
+
+//insert a new message
+router.post("/", (req, res) => {
+    const {
+        senderName,
+        senderMail,
+        receiverMail,
+        messageContent,
+    } = req.body;
+
+    if(!senderName || !senderMail || !receiverMail || !messageContent){
+        return res.status(400).json({
+            error: "All fields are required",
+        })
+    }
+
+
+    connection.query(`INSERT INTO messages (senderName, senderMail, receiverMail, messageContent) values (${mysql.escape(senderName)}, ${mysql.escape(senderMail)}, ${mysql.escape(receiverMail)}, ${mysql.escape(messageContent)})`, (err, results) =>{
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+
+        return res.json({
+            results,
+        })
+    })
+
+
+});
+
+//delete a message
+router.delete("/:id", (req, res) => {
+    const {id} = req.params;
+    connection.query(`DELETE FROM messages where entryID = ${mysql.escape(id)}`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            results,
+        })
+    })  
+});
+
+router.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const {
+        senderName,
+        senderMail,
+        receiverMail,
+        messageContent,
+    } = req.body;
+
+    if(!senderName || !senderMail || !receiverMail || !messageContent){
+        return res.status(400).json({
+            error: "All fields are required",
+        })
+    }
+
+    connection.query(`UPDATE messages SET senderName = ${mysql.escape(senderName)}, senderMail = ${mysql.escape(senderMail)}, receiverMail = ${mysql.escape(receiverMail)}, messageContent = ${mysql.escape(messageContent)} WHERE entryID = ${mysql.escape(id)}`, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.send(err);
+        }
+        return res.json({
+            results,
+        });
+    })
+});
+
+router.post("/foreign", async (req, res) => {
+    const {senderName, senderMail, receiverMail, messageContent, language} = req.body;
+
+    if(!senderName || !senderMail || !receiverMail || !messageContent || !language){
+        return res.status(400).json({
+            error: "All fields are required",
+        })
+    }
+
+    if(!LANGUAGE_ISO_CODE[language] && language != "ALL"){
+        return res.status(400).send("Invalid Language");
+    }
+
+    let transationData = {};
+
+    try {
+       if(LANGUAGE_ISO_CODE[language]){
+           const translatedText = await translateText(
+               messageContent,
+               LANGUAGE_ISO_CODE[language]
+        );   
+        transationData.translatedText = translatedText[0];
+    }
+     else if (language === "ALL"){
+            const availableLanguages = Object.values(LANGUAGE_ISO_CODE);
+            
+            const translatedAnswersArray = await Promise.all(
+                availableLanguages.map(async (language) => {
+                    const translatedText = await translateText(messageContent, language);
+                    return translatedText[0];
+                })
+            );
+
+            transationData.translatedText = translatedAnswersArray.reduce(
+                (acc, curr) => {
+                    return acc + curr + "\n"
+                }, ""
+            )
+        }
+        else {
+            return res.send("Invalid Language");
+        }
+
+        sendMail(receiverMail, senderMail, transationData.translatedText, `${senderName} has sent you a message`);
+
+        connection.query(`INSERT INTO messages (senderName, senderMail, receiverMail, messageContent) values (${mysql.escape(senderName)}, ${mysql.escape(senderMail)}, ${mysql.escape(receiverMail)}, ${mysql.escape(messageContent)})`, (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.send(err);
+            }
+
+            return res.json({
+                transationData
+            });
+        }
+    );
+    }catch(err){
+        console.log(err);
+        return res.send(err);
+    }
+});
+
+module.exports = router;
---
+++ router/utilsRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const {detectLanguage, translateText} = require("../utils/translateFunctions")
+const {sendMail } = require("../utils/mailFunctions");
+const {LANGUAGE_ISO_CODE} = require("../utils/dictionaries");
+
+router.get("/detect", async (req,res) => {
+    const {text} = req.body;
+
+    if(!text){
+        return res.status(400).send("Missing Parametres");
+    }
+
+   const languageDetection = await detectLanguage(text);
+   return res.json({
+       language: languageDetection[0]?.language
+   })
+});
+
+router.get("/translate", async (req,res) => {
+    const {text, language} = req.body;
+
+    if(!text || !language){
+        return res.status(400).send("Missing Parametres");
+    }
+
+    if(!LANGUAGE_ISO_CODE[language]){
+        return res.status(400).send("Invalid Language");
+    }
+
+   const translatedText = await translateText(text, language);
+   return res.json({
+    translatedText: translatedText[0]
+   })
+});
+
+router.post("/send", (req, res) => {
+    const {senderName, senderMail, receiverMail, messageContent} = req.body;
+    if(!senderName || !senderMail || !receiverMail || !messageContent) {
+        return res.status(400).send("Missing Parametres");
+    }
+
+    sendMail(receiverMail, senderMail, messageContent, `${senderName} has sent you a message`);
+    res.send(200);
+});
+
+module.exports = router;
